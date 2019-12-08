0df920957b74bb8e3f12bb070b40d5d8eeb252a3
zukaaman@gmail.com <zukaaman@gmail.com>
2019-12-08 10:21:31 +0400

    start

---
+++ src/blocks/modules/list/list.js
@@ -0,0 +1,13 @@
+modules.define('list', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
